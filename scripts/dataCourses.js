fd84b952edfd2a12ef68a17cdbbdef2fda32963e
Andres Rincon <af.rincon@uniandes.edu.co>
2020-10-21 23:58:34 -0500

    todo

---
+++ scripts/dataCourses.js
@@ -0,0 +1,6 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Rubby Casallas", 3),
+    new Course("TI", "Elizabeth Muñoz", 3),
+    new Course("Sistrans", "German Bravo", 3),
+];
